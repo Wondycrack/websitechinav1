cfd104494f2e7652b31850ea066e17704389fe68
Mickael Taillepierre <mick0987@hotmail.ch>
2022-02-28 13:18:09 +0800

    login

--- js/app.js
+++ js/app.js
@@ -43,28 +43,4 @@ contactForm.addEventListener('submit', (e)=>{
         
     }
     xhr.send(JSON.stringify(formData))
-})
-
-//jquery login//
-
-function show() {
-    var p = document.getElementById('pwd');
-    p.setAttribute('type', 'text');
-}
-
-function hide() {
-    var p = document.getElementById('pwd');
-    p.setAttribute('type', 'password');
-}
-
-var pwShown = 0;
-
-document.getElementById("eye").addEventListener("click", function () {
-    if (pwShown == 0) {
-        pwShown = 1;
-        show();
-    } else {
-        pwShown = 0;
-        hide();
-    }
-}, false);
+});
