17e7a5a547fbe4bf723994c29429d44ff4472c10
Mickael Taillepierre <mick0987@hotmail.ch>
2022-02-28 13:11:08 +0800

    login

--- js/app.js
+++ js/app.js
@@ -47,25 +47,24 @@ contactForm.addEventListener('submit', (e)=>{
 
 //jquery login//
 
-$('#login-button').click(function(){
-    $('#login-button').fadeOut("slow",function(){
-      $("#container").fadeIn();
-      TweenMax.from("#container", .4, { scale: 0, ease:Sine.easeInOut});
-      TweenMax.to("#container", .4, { scale: 1, ease:Sine.easeInOut});
-    });
-  });
-  
-  $(".close-btn").click(function(){
-    TweenMax.from("#container", .4, { scale: 1, ease:Sine.easeInOut});
-    TweenMax.to("#container", .4, { left:"0px", scale: 0, ease:Sine.easeInOut});
-    $("#container, #forgotten-container").fadeOut(800, function(){
-      $("#login-button").fadeIn(800);
-    });
-  });
-  
-  /* Forgotten Password */
-  $('#forgotten').click(function(){
-    $("#container").fadeOut(function(){
-      $("#forgotten-container").fadeIn();
-    });
-  });
+function show() {
+    var p = document.getElementById('pwd');
+    p.setAttribute('type', 'text');
+}
+
+function hide() {
+    var p = document.getElementById('pwd');
+    p.setAttribute('type', 'password');
+}
+
+var pwShown = 0;
+
+document.getElementById("eye").addEventListener("click", function () {
+    if (pwShown == 0) {
+        pwShown = 1;
+        show();
+    } else {
+        pwShown = 0;
+        hide();
+    }
+}, false);
